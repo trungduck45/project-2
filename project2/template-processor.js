b08b32035593d6e21272805c9ed62c86551b1c33
trungduck45 <letrungducabcxyz@gmail.com>
2024-03-03 10:36:43 +0700

    first commit

---
+++ project2/template-processor.js
@@ -0,0 +1,14 @@
+    "use strict";
+
+function TemplateProcessor(template) {
+  this.template = template;
+}
+
+TemplateProcessor.prototype.fillIn = function (dictionary) {
+  let filledTemplate = this.template.replace(/{{(.*?)}}/g, (match, property) => {
+    return dictionary[property] || "";
+  });
+  return filledTemplate;
+};
+
+module.exports = TemplateProcessor;
